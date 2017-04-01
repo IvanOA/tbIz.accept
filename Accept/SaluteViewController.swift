4690fd35e05d86912e74ce40b051b2f7a62c2c1c
Волков Никита <volkovnikita@MacBook-Pro-Volkov.local>
2017-04-01 12:38:39 +0300

    #9 3 windows added

---
+++ Accept/SaluteViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SaluteViewController.swift
+//  Accept
+//
+//  Created by Волков Никита on 01.04.17.
+//  Copyright © 2017 Иван. All rights reserved.
+//
+
+import UIKit
+
+class SaluteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
