39acd2b5adc9b979db884d9992cd9f0565b3972a
IvanOA <juzippee@mail.ru>
2017-04-01 12:01:30 +0300

    #3 Realm Installed

---
+++ Accept/LoadData.swift
@@ -0,0 +1,9 @@
+//
+//  LoadData.swift
+//  Accept
+//
+//  Created by Иван on 01.04.17.
+//  Copyright © 2017 Иван. All rights reserved.
+//
+
+import Foundation
--- Accept/ViewController.swift
+++ Accept/ViewController.swift
@@ -8,18 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainVC: UIViewController{
 
+    @IBOutlet var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
+extension MainVC: UITableViewDelegate, UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellPlace", for: indexPath)
+        
+        return cell
+    }
+    
+}
